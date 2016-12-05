2da9cc619b0ef924a4062eccdcb17b1ad58b474e
Joeelamor <xiaozhu930330@outlook.com>
2016-12-05 10:13:21 -0600

    upload 8 assignments

---
+++ Assignment/0903/Pair.java
@@ -0,0 +1,10 @@
+public static void main(String args[])
+{      
+   Pair<String> cr = new Pair<>();
+   cr.set1("This is the first sentence!");
+   cr.set2("This is the second sentence!");
+   String myMethod1 = cr1.get1();
+   System.out.println(myMethod1);
+   String myMethod2 = cr2.get2();
+   System.out.println(myMethod2);
+}
